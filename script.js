aa5548f8fe5e80b174b526ee5d3d703695132b1e
FedericoCarrasco <federicogcarrasco@gmail.com>
2021-10-11 19:51:40 -0300

    first commit: add index.html, script.js and create computerPlay function

---
+++ script.js
@@ -0,0 +1,13 @@
+function computerPlay () {
+    let computerChoice = Math.random();
+    if (computerChoice <= 0.34) {
+        return "Rock";
+    }
+    else if (computerChoice > 0.34 && computerChoice <= 0.67) {
+        return "Papel";
+    }
+    else {
+        return "Scissors";
+    }
+}
+
