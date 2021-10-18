b79df50de02aa5dffc24a97de5b3ccabf0aade4f
FedericoCarrasco <federicogcarrasco@gmail.com>
2021-10-18 20:41:01 -0300

    creating the UI

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-function computerPlay () {
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => playRound("rock"));
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => playRound("paper"));
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => playRound("scissors"));
+
+const computerWeapon = document.querySelector('#computerWeapon');
+
+const result = document.querySelector('#result');
+
+const playerCounter = document.querySelector('#counter1 p');
+let playerCounterNumber = 0;
+playerCounter.textContent = `${playerCounterNumber} times.`;
+
+const computerCounter = document.querySelector('#counter2 p');
+let computerCounterNumber = 0;
+computerCounter.textContent = `${computerCounterNumber} times.`;
+
+
+function computerPlay() {
     let randomNumber = Math.random();
-    if (randomNumber <= 0.34) {
+    if (randomNumber <= 0.33) {
         return "Rock";
     }
-    else if (randomNumber > 0.34 && randomNumber <= 0.67) {
+    else if (randomNumber > 0.33 && randomNumber <= 0.66) {
         return "Paper";
     }
     else {
@@ -11,36 +34,53 @@ function computerPlay () {
     }
 }
 
-function playRound () {
-    let playerChoice = prompt("Choose rock, paper or scissors").toLowerCase();
-    let computerChoice = computerPlay();
-
-    if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") {
-        return "To play pick rock, paper or scissors";
+function youWin() {
+    result.textContent = "You win the battle!";
+    playerCounterNumber += 1;
+    playerCounter.textContent = `${playerCounterNumber} times.`;
+    if (playerCounterNumber >= 5) {
+        playerCounterNumber = 0;
+        computerCounterNumber = 0;
+        result.textContent = `You have won the war!`;
     }
+}
 
-    console.log(`The computer choose ${computerChoice}`);
+function youLoose() {
+    result.textContent = "You loose the battle!";
+    computerCounterNumber += 1;
+    computerCounter.textContent = `${computerCounterNumber} times.`
+    if (computerCounterNumber >= 5) {
+        playerCounterNumber = 0;
+        computerCounterNumber = 0;
+        result.textContent = `The machine has won the war, fight again!`
+    }
+}
+
+function playRound (playerChoice) {
+
+    let computerChoice = computerPlay();
+    computerWeapon.textContent = `The computer choose ${computerChoice}`;
 
     if (playerChoice == "rock" && computerChoice == "Paper") {
-        return "You loose!";
+        youLoose();
     }
     else if (playerChoice == "rock" && computerChoice == "Scissors") {
-        return "You win!";
+        youWin();
     }
     else if (playerChoice == "paper" && computerChoice == "Scissors") {
-        return "You loose!";
+        youLoose();
     }
     else if (playerChoice == "paper" && computerChoice == "Rock") {
-        return "You win!";
+        youWin();
     }
     else if (playerChoice == "scissors" && computerChoice == "Rock") {
-        return "You loose!";
+        youLoose();
     }
     else if (playerChoice == "scissors" && computerChoice == "Paper") {
-        return "You win!";
+        youWin();
     }
     else {
-        return "It's a tie!";
+        result.textContent = "No one won this battle!";
     }
 }
 
